f71882ad192d3e0673afc41628108fca11799bf6
User9462 <gaetan.le-nestour@stcharles-stecroix.org>
2026-02-03 17:01:26 +0100

    Update Research.js

--- Research.js
+++ Research.js
@@ -1,30 +1,10 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <style>
-        body {
-            background-color: #090e22;
-        }
-    </style>
+const page = "a";
+const list = [page];
 
-    <form action="Research.js" method="post">
-        <center>
-            <img src="llmlogo.jpg" alt="HomePageLogo" width="600" height="350">  
-            <br>
-            <br>
-            <input name="InputSearch" type="text" placeholder="Rechercher.."> <br> <br>
-
-            <button type="submit">Envoyer</button>
-            <button type="reset">Annuler</button> <br> <br>
-            <a href="Help.html">
-                <button type="button">Besoin d'aide ?</button>
-            </a>
-        </center>
-    </form>
-</body>
-</html>
+function checkValue(userResearch) {
+    if (list.includes(userResearch)) {
+        window.location.href = "hello.html";
+    } else {
+        alert("Value NOT found in array");
+    }
+}
