7094ccca662ec31ad0f3ce00bffa3b6c926c44c7
User9462 <gaetan.le-nestour@stcharles-stecroix.org>
2026-02-03 16:01:30 +0100

    Update Research.php

--- Research.php
+++ Research.php
@@ -1,21 +1,13 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="Research.php" method="post">
-        <center>
-            <img src="llmlogo.jpg" alt="HomePageLogo" width="600" height="350">  
-            <br>
-            <br>
-            <input name="InputSearch" type="text" placeholder="Search.."> <br> <br>
+<?php
+$UserResearch = $_POST["BarResearch"];
 
-            <button type="submit">Envoyer</button>
-            <button type="reset">Annuler</button>
-        </center>
-    </form>
-</body>
-</html>
+$Page = "a";
+$List = [$Page];
+
+if (in_array($UserResearch, $List)) {
+    echo "Value found in array";
+    echo $UserResearch;
+} else {
+    echo "Value NOT found in array";
+}
+?>
