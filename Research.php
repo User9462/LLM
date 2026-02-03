ed543d44930c27647014a532d71679cb62ba0b01
User9462 <gaetan.le-nestour@stcharles-stecroix.org>
2026-02-03 15:57:37 +0100

    Create Research.php

---
+++ Research.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="Research.php" method="post">
+        <center>
+            <img src="llmlogo.jpg" alt="HomePageLogo" width="600" height="350">  
+            <br>
+            <br>
+            <input name="InputSearch" type="text" placeholder="Search.."> <br> <br>
+
+            <button type="submit">Envoyer</button>
+            <button type="reset">Annuler</button>
+        </center>
+    </form>
+</body>
+</html>
